34928f832022a6f5c8e5bbfaa2c078e7858592b6
LONLA_G <gatien.lonla@cca-bank.com>
2024-09-13 10:39:58 +0100

    add some settings

--- src/main/java/com/estate/repository/VisitorRepository.java
+++ src/main/java/com/estate/repository/VisitorRepository.java
@@ -3,16 +3,17 @@ package com.estate.repository;
 
 import com.estate.domain.entity.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface VisitorRepository extends JpaRepository <Visitor, Long> {
     Optional<Visitor> findByEmail(String email);
-    List<Visitor> findVisitorByCreationDateEquals(LocalDateTime localDateTime);
-
+    @Transactional
+    @Modifying(clearAutomatically = true)
+    void deleteAllByCreationDateBefore(LocalDateTime date);
 }
