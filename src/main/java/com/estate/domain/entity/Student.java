881395c22c56f37bd03f3cbe4eab14d649b29b83
lonla <gatienjordanlonlaep@gmail.com>
2026-03-24 21:18:11 +0100

    maven plugin issue fixed

--- src/main/java/com/estate/domain/entity/Student.java
+++ src/main/java/com/estate/domain/entity/Student.java
@@ -14,6 +14,7 @@ import org.hibernate.annotations.UuidGenerator;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import jakarta.persistence.*;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.*;
